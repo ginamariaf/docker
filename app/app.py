8049df7e289ba0d6f564a0f93bfb0de32ee01971
ginamariatf <s_faerber20@stud.hwr-berlin.de>
2024-05-17 15:59:44 +0200

    hzzt

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import redis
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='srv-captain--redis', port=6379)
 
 def get_hit_count():
     retries = 5
